06d764c85ac1b13182df4d991a7800ea01d2cb7e
nabeo1900 <89067319+nabeo1900@users.noreply.github.com>
2023-04-17 23:08:27 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,24 @@
     v-if="statusMask == 'default'"
     @onStart="onHandleBeforeStart($event)"
   />
-  <interact-screen v-if="statusMask == 'match'" />
+  <interact-screen
+    v-if="statusMask == 'match'"
+    :cardContext="settings.cardContext"
+  />
 </template>
 <script>
 import MainScreen from "./components/MainScreen.vue";
 import InteractScreen from "./components/InteractScreen.vue";
+import { shuffled } from "./utils/array";
 export default {
   name: "App",
   data() {
     return {
+      settings: {
+        totalOfBlocks: 0,
+        cardContext: [],
+        startedAt: null,
+      },
       statusMask: "default",
     };
   },
@@ -20,8 +29,18 @@ export default {
     InteractScreen,
   },
   methods: {
-    onHandleBeforeStart(config) {
-      console.log(config);
+    onHandleBeforeStart(configs) {
+      this.settings.totalOfBlocks = configs.totalOfBlocks;
+      const firstCard = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      console.log(firstCard);
+      const secondCard = [...firstCard];
+      const card = [...firstCard, ...secondCard];
+      this.settings.cardContext = shuffled(shuffled(shuffled(shuffled(card))));
+      this.settings.startedAt = new Date().getTime();
+
       this.statusMask = "match";
     },
   },
--- src/components/InteractScreen.vue
+++ src/components/InteractScreen.vue
@@ -1,12 +1,43 @@
 <template>
-  <card-flip />
+  <div class="sreen">
+    <div class="screen__inner">
+      <CardFlip v-for="(card, index) in cardContext" :key="index" />
+    </div>
+  </div>
 </template>
+
 <script>
 import CardFlip from "./Card.vue";
-
 export default {
-  component: {
+  props: {
+    cardContext: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+  },
+  components: {
     CardFlip,
   },
 };
 </script>
+<style scoped>
+.screen {
+  width: 100%;
+  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  background-color: var(--dark);
+  color: var(--light);
+}
+
+.screen__inner {
+  width: calc(424px);
+  display: flex;
+  flex-wrap: wrap;
+  margin: 2rem auto;
+}
+</style>
